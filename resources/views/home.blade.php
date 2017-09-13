627c84ea55ddec1b1d9667b4a9636d8c7600180d
Ménétrier Jérôme <menetrier.jerome@hotmail.com>
2017-09-13 15:09:10 +0200

    ^^

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <br><br>
-    <h3 id="titre3Ho"> Le jeu d'Echec est le Sport Intellectuel par Excellence ! </h3>
+    <h2 id="titre3Ho"> Le jeu d'Echec est le Sport Intellectuel par Excellence ! </h2>
     <br><br>
     <p id="presentation">
         Quel que soit votre Age, votre Niveau, votre Motivation, Fille ou Garçon…
@@ -13,6 +13,7 @@
         le Club d’Echecs d’Ollioules vous offrira par le biais de son Entraineur et de ses Adhérents,
         une Ambiance Conviviale et Enrichissante à l’Epanouissement Personnel !
     </p>
+    <br><br>
     <div class="row">
         <div class="col-lg-5">
 
@@ -26,16 +27,25 @@
         </div>
             <div class="col-lg-3">
             </div>
-                <div class="col-lg-4">
+                <div class="col-lg-3">
         <ul id="infCourante">
             <li> Réouverture du Club : </li>
             <li>  Mercredi 6 Septembre </li>
             <li> (1 Mois d’Essai Gratuit)</li>
+
         </ul>
                 </div>
+        <div class="col-lg-1">
+        </div>
     </div>
+    <br><br>
 
-    <h4 id="titre4Ho">Les passionnés du Club d’Ollioules seront honorés de vous accueillir le :</h4>
+    <h4 id="titre3Ho">Les passionnés du Club d’Ollioules seront honorés de vous accueillir le :</h4>
+    <br>
+    <div class="row">
+        <div class="col-lg-4">
+        </div>
+        <div class="col-lg-4">
     <ul id="hCourant">
         <li>Lundi : 16h30 - 20h30</li>
         <li>Mardi : 14h - 18h</li>
@@ -44,7 +54,11 @@
         <li>Vendredi : 16h30 - 20h30</li>
         <li>Samedi : 9h - 12h</li>
     </ul>
-    <h5 id="titre5Ho">Pour tous renseignements, n’hésitez pas à contacter l’Entraîneur du Club </h5>
+        </div>
+        <div class="col-lg-4">
+        </div>
+    </div>
+    <h4 id="titre5Ho">Pour tous renseignements, n’hésitez pas à contacter l’Entraîneur du Club </h4>
 <br><br><br>
 
 
