72a689ca77837b8a95e86107b95e7a4410a1a84a
jbdfjojo <jbdfjojo@hotmail.fr>
2017-09-13 11:50:06 +0200

    add

--- resources/views/compte.blade.php
+++ resources/views/compte.blade.php
@@ -3,42 +3,77 @@
 @section('title', 'Page Title')
 
 @section('content')
-    <h1 style="text-align: center; width: 100%">mon compte</h1><br><br>
 
     <?php if (isset($_SESSION['connexion']) && $_SESSION['connexion'] == 'ok') : ?>
+    <h1 style="text-align: center; width: 100%">mon compte</h1>
+    <a href="deco" style="float: right; margin-right: 10%"> deconnexion</a>
 
+    <br><br><br><br>
     <div id="mon_compte">
 
         <form action="#">
 
-            <select>
-                <option value="Mr">Mr</option>
-                <option value="Mme">Madame</option>
-                <option value="J_F">Jeune Fille</option>
-                <option value="J_H">Jeune Homme</option>
-            </select>
-            <br>
+            <div class="row">
 
-            <input type="text" name="nom" id="">
-            <input type="text" name="prenom" id=""><br>
+                <div class="col-lg-8">
+                    <label for="">Genre : </label><br>
+                    <select>
+                        <option value="Mr">Mr</option>
+                        <option value="Mme">Madame</option>
+                        <option value="J_F">Jeune Fille</option>
+                        <option value="J_H">Jeune Homme</option>
+                    </select>
+                    <br><br>
 
-            <input type="date" name="date" id=""><br>
-            <input type="text" name="categorie" id="" disabled><br>
-            <input type="text" name="pseudo" id=""><br>
-            <input type="password" name="pass" id=""><br>
-            <input type="password" name="confirm_pass" id=""><br>
-            <input type="email" name="mail" id=""><br>
-            <input type="tel" name="tel" id=""><br>
-            <textarea name="adresse" id="" cols="30" rows="3">
+                    <label for="">Nom :</label>
+                    <label for="" style="margin-left: 185px">Prenom :</label><br>
 
-            </textarea><br>
-            <input type="text" name="ffe" id=""><br>
+                    <input type="text" name="nom" id=""> &nbsp;&nbsp;&nbsp;
+                    <input type="text" name="prenom" id=""><br><br>
 
+                    <label for="">Date de naissance :</label><br>
+                    <input type="date" name="date" id=""><br><br>
 
+                    <label for="">Categorie :</label><br>
+                    <input type="text" name="categorie" id="" disabled><br><br>
+
+                    <label for="">Pseudo :</label><br>
+                    <input type="text" name="pseudo" id=""><br><br>
+
+                    <label for="">mot de passe :</label><br>
+                    <input type="password" name="pass" id=""><br><br>
+
+                    <label for="">Confirmation du mot de passe :</label><br>
+                    <input type="password" name="confirm_pass" id=""><br><br>
+
+                    <label for="">Mail :</label><br>
+                    <input type="email" name="mail" id=""><br><br>
+
+                    <label for="">Telephone :</label><br>
+                    <input type="tel" name="tel" id=""><br><br>
+
+                    <label for="">Adresse :</label><br>
+                    <textarea name="adresse" id="" cols="30" rows="3">
+
+                    </textarea><br><br>
+
+                    <label for="">code FFE :</label><br>
+                    <input type="text" name="ffe" id=""><br><br>
+                </div>
+
+                <div class="col-lg-4" style="text-align: center">
+                    <img src="img/avatar.PNG" alt="" ><br><br>
+                    <input type="file" name="" id="">
+                </div>
+
+            </div>
+
+            <input type="submit" value="valider">
+            <input type="submit" value="annuler">
 
         </form>
 
-        <a href="deco"> deconnexion</a>
+        <br><br><br><br><br><br>
     </div>
 
     <?php else: ?>
@@ -57,16 +92,16 @@
 
               ?>
 
-            <br><br>
+            <br><br><br><br>
 
-            <label for="">nom</label><br>
-            <input type="text" name="user" id="user" placeholder="entrer votre peseudo"><br>
+            <label for="">nom :</label><br>
+            <input type="text" name="user" id="user" placeholder="entrer votre peseudo"><br><br>
 
-            <label for="">mot de pass</label><br>
+            <label for="">mot de pass :</label><br>
             <input type="text" name="pass" id="pass" placeholder="entrer votre peseudo"><br><br>
 
-            <input type="submit">
-            <br><br><br>
+            <input type="submit" value="valider">
+            <br><br><br><br><br><br>
         </form>
 
     </div>
