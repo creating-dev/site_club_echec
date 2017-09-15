20d648c41405f5bba58f25f611d8ac85b4fbb5a3
jbdfjojo <jbdfjojo@hotmail.fr>
2017-09-15 08:58:47 +0200

    add

---
+++ database/DAO/sql/site_echec.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 13 Septembre 2017 à 09:01
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.0.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `site_echec`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id_users` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `token` varchar(255) NOT NULL,
+  `genre` char(1) NOT NULL,
+  `birthday` date NOT NULL,
+  `categorie` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `tel` char(10) DEFAULT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  `codeFFE` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id_users`, `nom`, `pass`, `avatar`, `token`, `genre`, `birthday`, `categorie`, `mail`, `tel`, `adresse`, `codeFFE`) VALUES
+(1, 'jojo', 'jojo', NULL, 'sfdsrrfhgs442424xfcvfgfd', 'F', '2017-09-07', 'poule', 'ghgfhfgfghfg@test.fr', '0901020304', '26 gfdgdfgfdgfdgdfg 90000 yyutyuytu', NULL);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_users`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
