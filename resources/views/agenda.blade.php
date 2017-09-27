9a8f8115f6fd2ca1ea56f056898fb14848a981ff
jbdfjojo <jbdfjojo@hotmail.fr>
2017-09-27 16:11:36 +0200

    add

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -38,17 +38,32 @@
 
                     <?php for ($i = 1; $i <= $calendrier[$j]['jour'] ; $i++): ?>
 
+                        <?php
+                            if ($i <= 7){
+                                $semaines = 1;
+                            }elseif ($i <= 14){
+                                $semaines = 2;
+                            }elseif ($i <= 21){
+                                $semaines = 3;
+                            }elseif ($i <= 28){
+                                $semaines = 4;
+                            }else{
+                                $semaines = 5;
+                            }
+                        ?>
+
                         <?php if ($i == 8 || $i == 15 || $i == 22 || $i == 29): ?>
+
                             <div class="col-lg-1">
                                 &nbsp;
                             </div>
 
                             <div class="col-lg-1">
-                                <a href="agenda?jours={{$i}}&mois={{$j}}#{{$i-1}}">{{$i}}</a>
+                                <a href="agenda?jours={{$i}}&mois={{$j}}&semaines={{$semaines}}">{{$i}}</a>
                             </div>
                         <?php else: ?>
                             <div class="col-lg-1">
-                                <a href="agenda?jours={{$i}}&mois={{$j}}#{{$i-1}}">{{$i}}</a>
+                                <a href="agenda?jours={{$i}}&mois={{$j}}&semaines={{$semaines}}">{{$i}}</a>
                             </div>
                         <?php endif; ?>
 
@@ -60,12 +75,14 @@
 
     </div>
 
-    <?php else: ?>
+    <?php elseif (!isset($_GET['jours'])): ?>
 
     <div class="row">
+        <div class="col-lg-12" id="view_agenda_mois_titre">
+            {{$calendrier[$_GET['mois']]['nom']}}
+        </div>
+        <?php for ($j = 1; $j <= $calendrier[$_GET['mois']]['jour'] ; $j++): ?>
         <div class="col-lg-6">
-
-            <?php for ($j = 1; $j <= $calendrier[$_GET['mois']]['jour'] ; $j++): ?>
 
             <div class="row agenda_mois">
                 <div class="col-lg-1">
@@ -88,11 +105,76 @@
                     </div>
                 </div>
             </div>
+        </div>
+        <?php endfor; ?>
+    </div>
 
+    <?php else: ?>
+
+    <div class="row">
+        <div class="col-lg-12" id="view_agenda_mois_titre">
+            {{$calendrier[$_GET['mois']]['nom']}}
+        </div>
+
+        <div class="col-lg-6">
+
+            <?php
+
+                $semaines = [
+                    1 => [
+                        'debut' => 1,
+                        'fin' => 7
+                    ],
+                    2 => [
+                        'debut' => 8,
+                        'fin' => 14
+                    ],
+                    3 => [
+                        'debut' => 15,
+                        'fin' => 21
+                    ],
+                    4 => [
+                        'debut' => 22,
+                        'fin' => 28
+                    ],
+                    5 => [
+                        'debut' => 29,
+                        'fin' => $calendrier[$_GET['mois']]['jour']
+                    ]
+                ]
+
+            ?>
+
+            <?php for ($j = $semaines[$_GET['semaines']]['debut']; $j <= $semaines[$_GET['semaines']]['fin'] ; $j++): ?>
+                <div class="row agenda_mois">
+                    <div class="col-lg-1">
+                        &nbsp;
+                    </div>
+                    <div class="col-lg-1" style="background-color: #3097D1">
+                        <div id="{{$j}}" class="agenda_view_mois">
+                            {{$j}}
+                        </div>
+                    </div>
+                    <div class="col-lg-10" style="background-color: #cbb956 ">
+                        <div class="row">
+                            <?php for ($i = 1; $i <= 6; $i++): ?>
+                            <div class="col-lg-4">
+                                <div class="carte_event">
+                                    event {{$i}}
+                                </div>
+                            </div>
+                            <?php endfor; ?>
+                        </div>
+                    </div>
+                </div>
             <?php endfor; ?>
         </div>
         <div class="col-lg-6">
-           view event
+
+            <div id="view_agenda_event_titre">
+                
+            </div>
+
         </div>
     </div>
 
