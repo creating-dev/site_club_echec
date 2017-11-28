1fa67c7a13d001054bfedd1ee8346c09c8297f1b
jbdfjojo <jbdfjojo@hotmail.fr>
2017-11-28 21:54:30 +0100

    add

--- Forum/cache/production/sql_efd26735c3bcbef398e23fb85e7ccd99.php
+++ Forum/cache/production/sql_efd26735c3bcbef398e23fb85e7ccd99.php
@@ -1,13 +1,5 @@
 <?php exit; ?>
-<<<<<<< HEAD
-<<<<<<< HEAD
-1510769107
-=======
-1510768505
->>>>>>> dd359eb13c6b2bd45248214a2db2604c1cf3b75b
-=======
-1511813788
->>>>>>> jojo
+1511902971
 SELECT forum_id FROM phpbb_forums WHERE forum_options & 2 <> 0 LIMIT 1
 6
 a:0:{}
