d431c7d5f82b787e43008916b01fdd6cf3605ad7
jbdfjojo <jbdfjojo@gmail.com>
2018-07-10 17:20:01 +0200

    add

--- site/storage/framework/views/9d28cb6d36d8853d0a17d9080ecabedbd3a58c6e.php
+++ site/storage/framework/views/9d28cb6d36d8853d0a17d9080ecabedbd3a58c6e.php
@@ -3,8 +3,35 @@
 <?php echo $__env->make('sous_menu.sous_menu_club', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
 
 <?php $__env->startSection('content'); ?>
-    <h4 id="titre3Ho">Page membres en cours de réalisation merci de votre compréhension</h4>
-    <hr class="titr">
-    <br><br>
+
+
+        <table class="table table-hover liste_membre">
+            <thead>
+                <tr>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>john@example.com</td>
+                </tr>
+                <tr>
+                    <td>Mary</td>
+                    <td>Moe</td>
+                    <td>mary@example.com</td>
+                </tr>
+                <tr>
+                    <td>July</td>
+                    <td>Dooley</td>
+                    <td>july@example.com</td>
+                </tr>
+            </tbody>
+        </table>
+
+
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('template/default', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
