004d2a7166dac0cb243f0bf1d4ee628651dfe7d6
jbdfjojo <jbdfjojo@gmail.com>
2018-07-10 16:39:52 +0200

    add

--- site/resources/views/template/default.blade.php
+++ site/resources/views/template/default.blade.php
@@ -75,8 +75,7 @@
                         <ul>
                             <li><h3><u><a href="goodies">Goodies</a></u></h3></li>
                             <li>&nbsp;</li>
-                            <li><a href="goodies">Test</a></li>
-                            <li><a href="goodies">Test</a></li>
+                            <li><a href="goodies">goodies</a></li>
                         </ul>
                     </div>
 
@@ -84,8 +83,7 @@
                         <ul>
                             <li><h3><u><a href="forum">Forum</a></u></h3></li>
                             <li>&nbsp;</li>
-                            <li><a href="forum">Test</a></li>
-                            <li><a href="forum">Test</a></li>
+                            <li><a href="forums">forum</a></li>
                         </ul>
                     </div>
 
