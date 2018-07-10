2146a68ef7791149ff3a2a5cff3f2037b1a3a2c5
Ménétrier Jérôme <menetrier.jerome@hotmail.com>
2018-07-10 15:05:16 +0200

    mise a jours
    mise a jours BDD

--- site/database/sql/site_echec.sql
+++ site/database/sql/site_echec.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 06 Novembre 2017 à 11:52
--- Version du serveur :  5.7.11
--- Version de PHP :  7.0.4
+-- Généré le :  Mar 10 Juillet 2018 à 13:02
+-- Version du serveur :  5.7.14
+-- Version de PHP :  7.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
